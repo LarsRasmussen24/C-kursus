39061047fc7cdb45cee67a24b7c9395bd6a4e911
Lars <admin@TA-828-04>
2018-10-29 15:52:16 +0100

    rettet af LSR

---
+++ Opgave/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 10;
+
+            a++;
+
+            a--;
+
+            a += 20;
+
+            Console.WriteLine(a);
+            
+
+            double b=12.5;
+
+            b++;
+            b--;
+
+            b *= 2;
+
+            Console.WriteLine(b);
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
---
+++ VarDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VarDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a;
+
+
+            int lars = 1;
+
+
+
+
+            Console.WriteLine ("dur det?");
+            Console.ReadKey();
+
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            
+            Console.WriteLine("ja, og nu i grøn..");
+
+            Console.ReadKey();
+
+        }
+    }
+}
